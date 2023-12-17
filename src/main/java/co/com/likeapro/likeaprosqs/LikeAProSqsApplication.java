ba498bec50a71b023b3af3483c31841d5b608ce6
hehoyos <hehoyos@bancolombia.com.co>
2023-12-16 20:39:45 -0500

    Initial commit

---
+++ src/main/java/co/com/likeapro/likeaprosqs/LikeAProSqsApplication.java
@@ -0,0 +1,13 @@
+package co.com.likeapro.likeaprosqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeAProSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeAProSqsApplication.class, args);
+	}
+
+}
